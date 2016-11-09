e5285325ed1b1a847b3b79192ee2dc1a9761aab0
ToM <tom@leloop.org>
2016-11-09 17:26:56 +0100

    List .zip files instead of .zim files.

--- kiwix-list-latest.bash
+++ kiwix-list-latest.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for ENTRY in `rsync --recursive --list-only download.kiwix.org::download.kiwix.org/zim/ | \
-    grep ".zim" | grep -F -v '_nopic_' | tr -s ' ' | cut -d ' ' -f5 | sort -r` ; do
-    RADICAL=`echo $ENTRY | sed 's/_20[0-9][0-9]-[0-9][0-9]\.zim//g'`
+for ENTRY in `rsync --recursive --list-only download.kiwix.org::download.kiwix.org/portable/ | \
+    grep ".zip" | grep -F -v '_nopic_' | tr -s ' ' | cut -d ' ' -f5 | sort -r` ; do
+    RADICAL=`echo $ENTRY | sed 's/_20[0-9][0-9]-[0-9][0-9]\.zip//g'`
     if [[ $LAST != $RADICAL ]] ; then
         echo $ENTRY
         LAST=$RADICAL
